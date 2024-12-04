b3e71e9844a54a4edb4ea12f74d317c901d2f5bb
James Stockton <14148070+jamesstocktonj1@users.noreply.github.com>
2024-12-04 22:44:35 +0000

    Add greeting component

---
+++ components/endpoint/main.go
@@ -0,0 +1,30 @@
+//go:generate go run github.com/bytecodealliance/wasm-tools-go/cmd/wit-bindgen-go generate --world endpoint --out gen ./wit
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/jamesstocktonj1/wasmcloud-template/components/endpoint/gen/jamesstocktonj1/wasmcloud-template/connect"
+	"go.wasmcloud.dev/component/net/wasihttp"
+)
+
+func init() {
+	wasihttp.HandleFunc(handleRequest)
+}
+
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+	res := connect.Greet(connect.Request{
+		Message: "Hello",
+		Name:    "World",
+	})
+
+	enc := json.NewEncoder(w)
+	enc.SetIndent("", "  ")
+	enc.Encode(map[string]string{
+		"message": res,
+	})
+	w.WriteHeader(http.StatusOK)
+}
+
+func main() {}
